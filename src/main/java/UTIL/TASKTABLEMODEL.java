c44816487c31144970179ac18084c6bfc95fc8cc
Victor Cézar Bonatti <victorcezarbonatti@hotmail.com>
2022-09-15 21:07:48 -0300

    Evolução do projeto To.do App semana 6

--- src/main/java/UTIL/TASKTABLEMODEL.java
+++ src/main/java/UTIL/TASKTABLEMODEL.java
@@ -71,7 +71,7 @@ public class TASKTABLEMODEL extends AbstractTableModel {
                 return "";*/
 
             default:
-                return "Dados não encontrados";
+                return "Dados não encontrados" ;
 
         }
     }
